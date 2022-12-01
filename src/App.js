1c7974552f1f4d300eaec1e12768a889c8314470
Kajal lathiya <kajalbodarya77@gmail.com>
2022-12-01 21:02:02 +0530

    add favorite list in app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import MainSearch from './components/MainSearch'
 import CompanySearchResults from './components/CompanySearchResults'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
+import Favourites from './components/favourites';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainSearch />} />
         <Route path="/:companyName" element={<CompanySearchResults />} />
+        <Route path="/:favourites" element={<Favourites />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,20 +1,41 @@
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-)
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
+  const array = useSelector((state) => state.favorite);
+  console.log("array-->", array);
 
-export default Job
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={8}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+      <Col xs={1}>
+        <Button
+          onClick={() => {
+            dispatch({
+              type: "ADD_TO_FAVORITE",
+              payload: data
+            });
+          }}
+        >
+          <FaRegHeart />
+          {/* <FaHeart /> */}
+        </Button>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
